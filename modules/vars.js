4d4a8e9633414c27d09d4101488a50a13361df2d
Aman Singh Katal <besoeasy@users.noreply.github.com>
2025-09-16 00:11:35 +0530

    -

--- modules/vars.js
+++ modules/vars.js
@@ -2,7 +2,7 @@ import path from "path";
 import os from "os";
 
 export const SAVE_DIR = path.join(os.tmpdir(), "katal");
-export const SERVERPORT = process.env.SERVERPORT || 6799;
-export const WEBPORT = process.env.WEBPORT || 6798;
-export const SMBPORT = process.env.SMBPORT || 445;
+export const SERVERPORT = 6799;
+export const WEBPORT =  6798;
+export const SMBPORT =  445;
 
