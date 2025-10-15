3adc7e5971394f4c6381841afd0a1d2aaf6a6add
besoeasy <besoeasy@users.noreply.github.com>
2025-10-16 03:11:23 +0530

    fix: Update version to 0.0.7 and use environment variables for server ports

--- modules/vars.js
+++ modules/vars.js
@@ -2,6 +2,6 @@ import path from "path";
 import os from "os";
 
 export const SAVE_DIR = path.join(os.tmpdir(), "katal");
-export const SERVERPORT = 6799;
-export const WEBPORT = 6798;
-export const SMBPORT = 445;
+export const SERVERPORT = process.env.SERVERPORT || 6799;
+export const WEBPORT = process.env.WEBPORT || 6798;
+export const SMBPORT = process.env.SMBPORT || 445;
